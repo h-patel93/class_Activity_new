7fcee464d82600fe141a73a4907d0ea8c5fdef09
Harsh_Patel <harshyovaghri cchhe>
2019-06-11 09:55:07 -0400

    initial commit 1

--- app/src/main/java/com/example/a1893681/class_activity_new/Activity_DashBoard.java
+++ app/src/main/java/com/example/a1893681/class_activity_new/Activity_DashBoard.java
@@ -1,13 +1,27 @@
 package com.example.a1893681.class_activity_new;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Activity_DashBoard extends AppCompatActivity {
-
+    String name,pass;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__dash_board);
+        Intent i = getIntent();
+        name = i.getStringExtra("un");
+        pass = i.getStringExtra("ps");
+        TextView txt_name,txt_password;
+
+        txt_name = findViewById(R.id.lbl_username);
+        txt_password = findViewById(R.id.lbl_password);
+        txt_name.setText(name);
+        txt_password.setText(pass);
+
+
     }
 }
--- app/src/main/java/com/example/a1893681/class_activity_new/MainActivity.java
+++ app/src/main/java/com/example/a1893681/class_activity_new/MainActivity.java
@@ -20,19 +20,20 @@ public class MainActivity extends AppCompatActivity {
         edt_password=findViewById(R.id.edt_password);
         edt_username=findViewById(R.id.edt_username);
         btn_submit=findViewById(R.id.btn_submit);
+
         btn_submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String uname,pass;
-                uname= edt_username.toString();
-                pass=edt_password.toString();
+                uname= edt_username.getText().toString();
+                pass=edt_password.getText().toString();
                 Intent i = new Intent(MainActivity.this,Activity_DashBoard.class);
                 i.putExtra("un",uname);
                 i.putExtra("ps",pass);
 
 
                 startActivity(i);
-                Toast.makeText(getApplicationContext(),uname+"Thanks!!",Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(),uname + "Thanks!!",Toast.LENGTH_LONG).show();
 
             }
         });
