710c06d8de11580b12230e3e6158a57af7304b30
Harsh_Patel <harshyovaghri cchhe>
2019-06-11 10:32:32 -0400

    Alert dialog message done

--- app/src/main/java/com/example/a1893681/class_activity_new/MainActivity.java
+++ app/src/main/java/com/example/a1893681/class_activity_new/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.a1893681.class_activity_new;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -21,19 +23,44 @@ public class MainActivity extends AppCompatActivity {
         edt_username=findViewById(R.id.edt_username);
         btn_submit=findViewById(R.id.btn_submit);
 
+            final AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+
+
+
         btn_submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String uname,pass;
-                uname= edt_username.getText().toString();
-                pass=edt_password.getText().toString();
-                Intent i = new Intent(MainActivity.this,Activity_DashBoard.class);
-                i.putExtra("un",uname);
-                i.putExtra("ps",pass);
+                alertDialog.setMessage(R.string.alert);
 
 
-                startActivity(i);
-                Toast.makeText(getApplicationContext(),uname + "Thanks!!",Toast.LENGTH_LONG).show();
+                alertDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String uname,pass;
+                        uname= edt_username.getText().toString();
+                        pass=edt_password.getText().toString();
+                        Intent i = new Intent(MainActivity.this,Activity_DashBoard.class);
+                        i.putExtra("un",uname);
+                        i.putExtra("ps",pass);
+
+
+                        startActivity(i);
+                        Toast.makeText(getApplicationContext(),uname + "Thanks!!",Toast.LENGTH_LONG).show();
+
+
+
+                    }
+                });
+
+                alertDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getApplicationContext(), "Harsh dhanji patel", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
+            alertDialog.show();
 
             }
         });
